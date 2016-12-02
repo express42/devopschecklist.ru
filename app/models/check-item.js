d74f7e00fb700fc5b9da750b5f3c93d68d46ba39
Vitaly Khabarov <vitalykhabarov@vitalys-mbp.msk.bonasource.ru>
2016-12-02 15:46:05 +0300

    Little refactoring Added score calculation Fixed "showDescription" Changed css (somehing fixed, something broke)

--- app/models/check-item.js
+++ app/models/check-item.js
@@ -5,29 +5,5 @@ export default DS.Model.extend({
     description: DS.attr(),
     checked: DS.attr(),
     tags: DS.attr(),
-    category: DS.attr(),
-
-    isAlignment: Ember.computed('category', function() {
-        return this.get('category') === 'alignment';
-    }),
-
-    isContext: Ember.computed('category', function() {
-        return this.get('category') === 'context';
-    }),
-
-    isLearning: Ember.computed('category', function() {
-        return this.get('category') === 'learning';
-    }),
-
-    isLifecycle: Ember.computed('category', function() {
-        return this.get('category') === 'lifecycle';
-    }),
-
-    isOrganization: Ember.computed('category', function() {
-        return this.get('category') === 'organisation';
-    }),
-
-    isProcess: Ember.computed('category', function() {
-        return this.get('category') === 'process';
-    }),
+    category: DS.belongsTo("category"),
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model() {
-        return this.get('store').peekAll('check-item');
+        return this.get('store').peekAll('category');
     }
 });
